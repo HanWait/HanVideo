617b9d1a87a4ed0c9422093223a3f57bf3f80f45
HanWaitingCode <573544241@qq.com>
2018-03-07 21:32:02 +0800

    Initial Commit

---
+++ HanVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HanVideo
+//
+//  Created by 123 on 2018/3/7.
+//  Copyright © 2018年 Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HanVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HanVideo
+//
+//  Created by 123 on 2018/3/7.
+//  Copyright © 2018年 Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
